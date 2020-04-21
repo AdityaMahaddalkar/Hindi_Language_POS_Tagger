0d1f6e4eb741b679605f972e335681eab572b150
A M <adityam1311@gmail.com>
2020-04-21 17:20:50 +0530

    Added testing requirements

--- setup.py
+++ setup.py
@@ -10,11 +10,11 @@ with open('README.md') as readme_file:
 with open('HISTORY.rst') as history_file:
     history = history_file.read()
 
-requirements = ['pandas' ]
+requirements = ['pandas']
 
 setup_requirements = ['pytest-runner', ]
 
-test_requirements = ['pytest>=3', 'codecov']
+test_requirements = ['pytest>=3', 'pytest-cov']
 
 setup(
     author="Aditya Mahaddalkar",
